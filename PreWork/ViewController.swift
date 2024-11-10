5c6e0e18a701926a9f6ecd8531db557d17c0ac13
Alonna <strongalonna@gmail.com>
2024-11-09 22:24:18 -0500

    Initial Commit

---
+++ PreWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreWork
+//
+//  Created by Alonna on 11/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
